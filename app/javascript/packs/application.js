198a3c035a4ff5831d0ff621f6363c54b3530f76
Yas la menace <yasbernays@gmail.com>
2020-06-11 23:24:31 +0200

    upgrade

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,15 @@
 import "bootstrap";
-import { initUpdateNavbarOnScroll } from '../components/navbar';
 
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
   initUpdateNavbarOnScroll();
 });
+
+import { loadDynamicBannerText } from '../components/banner';
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  // [...]
+  loadDynamicBannerText();
+});
+console.log("Hello from app/javascript/packs/application.js!");
